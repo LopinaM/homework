fc0caaba51af0d74a5991f2d823eb6c4acb00f2c
LopinaM <rin.ElliA@yandex.ru>
2025-03-01 12:05:39 +0500

    changes for homework 11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import s from "./HW11.module.css";
 import s2 from "../../s1-main/App.module.css";
 import { restoreState } from "../hw06/localStorage/localStorage";
@@ -17,16 +17,26 @@ function HW11() {
     restoreState<number>("hw11-value2", 100)
   );
 
-  const change = (event: Event, value: number | Array<number>) => {
-    // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
-    if (Array.isArray(value)) {
-      setValue1(value[0]);
-      setValue2(value[1]);
+  //   const change = (event: Event, value: number | number[]) => {
+  //     // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
 
-      return;
-    }
+  //     if (Array.isArray(value)) {
+  //       setValue1(value[0]);
+  //       setValue2(value[1]);
+  //     } else {
+  //       setValue1(value);
+  //     }
+  //   };
 
-    setValue1(value);
+  const change = (event: Event, value: number | number[]) => {
+    requestAnimationFrame(() => {
+      if (Array.isArray(value)) {
+        setValue1(value[0]);
+        setValue2(value[1]);
+      } else {
+        setValue1(value);
+      }
+    });
   };
 
   return (
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -5,8 +5,28 @@ const SuperRange: React.FC<SliderProps> = (props) => {
   return (
     <Slider
       sx={{
+        width: "150px",
         // стили для слайдера // пишет студент
-        color: "#00CC22",
+        // color: "#00CC22",
+        // color: "#00CC22"
+        "& .MuiSlider-rail": {
+          // Линия
+          backgroundColor: "#8B8B8B", // Серый цвет линии (когда не заполнено)
+        },
+        "& .MuiSlider-track": {
+          // Заполненная часть
+          backgroundColor: "#00CC22", // Зелёная заполненная часть
+        },
+        "& .MuiSlider-thumb": {
+          // Кружочек
+          width: 18,
+          height: 18,
+          backgroundColor: "white", // Внешний круг белый
+          border: "6px solid #00CC22", // Внутренний кружочек зелёный
+          "&:hover, &:focus-visible": {
+            boxShadow: "0px 0px 0px 8px rgba(0, 204, 34, 0.16)", // Лёгкое свечение при наведении
+          },
+        },
       }}
       {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
     />
