833a15de2af8a79c5021bb660517f39c46a3a5d6
LopinaM <rin.ElliA@yandex.ru>
2025-03-02 15:20:45 +0500

    added homework 12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,28 @@
 const initState = {
-    themeId: 1,
-}
+  themeId: 1,
+};
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
+export const themeReducer = (
+  state = initState,
+  action: ThemeActionType
+): typeof initState => {
+  // fix any
+  switch (action.type) {
+    // дописать
+    case "SET_THEME_ID":
+      return { ...state, themeId: action.id };
 
-        default:
-            return state
-    }
-}
+    default:
+      return state;
+  }
+};
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type ThemeActionType = {
+  type: "SET_THEME_ID";
+  id: number;
+};
+
+export const changeThemeId = (id: number): ThemeActionType => ({
+  type: "SET_THEME_ID",
+  id,
+}); // fix any
